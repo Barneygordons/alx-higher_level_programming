931a659ec4d1a9299c41ce480dd577cf68912c5c
Barnabas <barnygordons@gmail.com>
2023-01-12 16:14:26 +0000

    Made new effective changes

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """ read a file """
 
+python3 -c 'print(__import__("my_module").__doc__)'
+python3 -c 'print(__import__("my_module").my_function.__doc__)'
 
 def read_file(filename=""):
     with open(filename,  encoding='utf-8') as f:
--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """ write a file"""
 
+python3 -c 'print(__import__("my_module").__doc__)'
+python3 -c 'print(__import__("my_module").my_function.__doc__)'
 
 def append_write(filename="", text=""):
     n = 0
