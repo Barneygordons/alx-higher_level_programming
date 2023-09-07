118e3b97c73322e7f264fa2040bf26bef9fb7b76
Barneygordons <barnygordons@gmail.com>
2023-09-06 17:24:41 -0700

    write a script that takes in arguments and displays all values in the states table of hbtn_0e_0_usa where name matches the argument. But this time, write one that is safe from MySQL injections!

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" write a script that takes in arguments and displays all values,
+in the states table of hbtn_0e_0_usa where name matches the,
+argument. But this time, write one that is safe from MySQL injections!
+"""
+
+if __name__ == "__main__":
+    import MySQLdb
+    from sys import argv
+    db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=argv[1],
+            passwd=argv[2],
+            db=argv[3])
+    cur = db.cursor()
+    cmd = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
+    cur.execute(cmd, (argv[4],))
+    states = cur.fetchall()
+    for state in states:
+        print(state)
+    cur.close()
+    db.close()
